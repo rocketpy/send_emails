3e006648eb0b012cf2124f9f2691df8fa6662ef1
rocketpy <dimitr.ushakov@gmail.com>
2020-02-02 23:29:41 +0200

    modified

--- example_4.py
+++ example_4.py
@@ -1,1 +1,48 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+from os.path import basename
 
+
+def send_mail(send_from: str, subject: str, text: str, 
+send_to: list, files= None):
+
+    send_to= default_address if not send_to else send_to
+
+    msg = MIMEMultipart()
+    msg['From'] = send_from
+    msg['To'] = ', '.join(send_to)  
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(text))
+
+    for f in files or []:
+        with open(f, "rb") as fil: 
+            ext = f.split('.')[-1:]
+            attachedfile = MIMEApplication(fil.read(), _subtype = ext)
+            attachedfile.add_header(
+                'content-disposition', 'attachment', filename=basename(f) )
+        msg.attach(attachedfile)
+
+
+    smtp = smtplib.SMTP(host="smtp.gmail.com", port= 587) 
+    smtp.starttls()
+    smtp.login(username,password)
+    smtp.sendmail(send_from, send_to, msg.as_string())
+    smtp.close()
+    
+    
+    #  USAGE
+    """
+    username = 'my-address@gmail.com'
+password = 'top-secret'
+default_address = ['my-address2@gmail.com'] 
+
+send_mail(send_from= username,
+subject="test",
+text="text",
+send_to= None,
+files= # pass a list with the full filepaths here...
+)
+    """
