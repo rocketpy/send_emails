62d1e6c38b7570ee8d30256cfe0b2972ba4fce58
rocketpy <dimitr.ushakov@gmail.com>
2020-02-03 21:58:04 +0200

    modified

--- short_script.py
+++ short_script.py
@@ -1,1 +1,23 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
 
+
+msg = MIMEMultipart()
+
+password = "password"
+msg['From'] = "from_address"
+msg['To'] = "to_address"
+msg['Subject'] = "Attached Photo"
+msg.attach(MIMEImage(file("abc.jpg").read()))
+file = "file path"
+fp = open(file, 'rb')
+img = MIMEImage(fp.read())
+fp.close()
+msg.attach(img)
+server = smtplib.SMTP('smtp.gmail.com: 587')
+server.starttls()
+server.login(msg['From'], password)
+server.sendmail(msg['From'], msg['To'], msg.as_string())
+server.quit()
