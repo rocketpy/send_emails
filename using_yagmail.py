34b614e5ea5e162f0d7b35c1c556738f75a73b58
rocketpy <dimitr.ushakov@gmail.com>
2020-02-04 21:49:17 +0200

    modified

--- using_yagmail.py
+++ using_yagmail.py
@@ -1,1 +1,35 @@
+#  https://pypi.org/project/yagmail/
+#  pip install yagmail
 
+
+import yagmail
+
+
+receiver = "your@gmail.com"
+body = "Hello World !"
+filename = "file_name.pdf"
+
+yag = yagmail.SMTP("my_email@gmail.com")
+yag.send(
+    to=receiver,
+    subject="Test with attachment",
+    contents=body, 
+    attachments=filename,
+)
+
+
+
+#  example taked from official docs !!!
+import yagmail
+
+
+yag = yagmail.SMTP()  # yag = yagmail.SMTP('mygmailusername', 'mygmailpassword')
+
+contents = [
+    "This is the body, and here is just text http://somedomain/image.png",
+    "You can find an audio file attached.", '/local/path/to/song.mp3'
+]
+yag.send('to@someone.com', 'subject', contents)
+
+# Alternatively, with a simple one-liner:
+yagmail.SMTP('mygmailusername').send('to@someone.com', 'subject', contents)
