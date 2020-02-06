722b440e62117deab4b5b4160cd9a2593e6d9385
rocketpy <dimitr.ushakov@gmail.com>
2020-02-06 23:03:57 +0200

    modified

--- sending_multiple_emails.py
+++ sending_multiple_emails.py
@@ -1,1 +1,22 @@
+import csv
+import ssl
+import smtplib 
 
+
+message = "Hi {name}, your grade is {grade}"
+
+my_address = "my_email@gmail.com"
+my_password = input("Type your password and press enter: ")
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+    server.login(my_address, my_password)
+    
+    with open("file_name.csv") as file:
+        reader = csv.reader(file)
+        next(reader)  # skip header row
+        for name, email, grade in reader:
+            server.sendmail(from_address,
+                            email,
+                            message.format(name=name,grade=grade))
